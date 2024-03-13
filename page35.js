e86805a248a94d61b996b5ac98e2b4f9c10be6af
ilansav25 <ilan84988@gmail.com>
2024-03-13 19:09:09 +0200

    class ex

---
+++ page35.js
@@ -0,0 +1,9 @@
+var n = +prompt("enter the number to get number added with number 3:")
+var sum = 0;
+for (var index = 0; index <= n; index++) {
+    if(index % 3 == 0){
+        sum = sum + index
+    }
+    
+}
+console.log(sum)
