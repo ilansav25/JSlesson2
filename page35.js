0c7cbfc47a65f42465a98e15984ad55ac9bcfa13
ilansav25 <ilan84988@gmail.com>
2024-03-13 21:01:07 +0200

    class ex second fix

--- page35.js
+++ page35.js
@@ -1,8 +1,17 @@
-var n = +prompt("enter the number to get number added with number 3:")
+//ex 4 
+/*  var n = +prompt("enter the number to get number added with number 3:")
 var sum = 0;
 for (var index = 0; index <= n; index++) {
     if(index % 3 == 0){
         sum = sum + index
     }
+}
+console.log(sum)  */
+
+//ex 6 
+var num = +prompt("Enter sum number")
+var sum = 0;
+for (var index = 0; index <= num; index++) {
+    sum = sum + Math.pow(2 , index)
 }
 console.log(sum)
