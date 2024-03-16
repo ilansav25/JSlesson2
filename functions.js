58ba66d94ad22f24058381bd8b9e2219b0bdddf6
ilansav25 <ilan84988@gmail.com>
2024-03-16 19:26:15 +0200

    new promts

--- functions.js
+++ functions.js
@@ -33,7 +33,24 @@ function getCartAfterDiscount(totalPrice, discount) {
 console.log(getCartAfterDiscount(50, 0.1))
 console.log(getCartAfterDiscount(300, 0.2))
 
-function totalTime(Hours, Minutes, Seconds) {
-    return "Hours: " + " " + Hours + " " + "Minutes: " + " " +  Minutes + " " +  "Seconds" + " " + Seconds
+function getClock(hours, min, sec) {
+    if (hours < 24 && hours >= 0 && min >= 0 && min < 60 && sec >= 0 && sec < 60) {
+        if (hours < 10) {
+            hours = "0" + hours
+        }
+        if (min < 10) {
+            min = "0" + min
+        }
+        if (sec < 10) {
+            sec = "0" + sec
+        }
+        return hours + ":" + min + ":" + sec
+    } else {
+        return "Wrong input"
+    }
 }
-console.log(totalTime(3, 30, 0))
+
+console.log(getClock(12, 20, 55))
+console.log(getClock(5, 20, 12))
+console.log(getClock(6, 2, 55))
+console.log(getClock(8, 1, 55))
